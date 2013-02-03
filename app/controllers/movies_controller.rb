6b0c40caa24a404a99a213b898082b7c0916820a
Kostas Georgiadis <kongeor@gmail.com>
2013-02-03 13:29:03 +0200

    Exercise 1, done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params['sort_by'] == "title"
+      @movies = Movie.order('title')
+    elsif params['sort_by'] == "release_date"
+      @movies = Movie.order('release_date')
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
